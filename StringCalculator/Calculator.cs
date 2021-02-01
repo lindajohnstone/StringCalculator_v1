57bc5e2c4f5a52d4ef7013f5a20ec0969f307fa2
Linda Johnstone <linda.johnstone@myob.com>
2021-02-01 11:25:55 +1100

    Refactoring

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -9,28 +9,39 @@ namespace StringCalculator
         {
         }
 
-        public int Add(string v)
+        public int Add(string integers)
         {
-            if (v != " ") 
+            if (integers != " ")
             {
                 var sum = 0;
-                var numbers = v.Split(',', '\n', ';');
+                var numbers = integers.Split(',', '\n', ';');
                 var numList = new List<int>();
-                foreach (string number in numbers)
-                {
-                    Int32.TryParse(number, out var num);
-                    if (num < 0) numList.Add(num);
-                    sum = sum + num;
-                }
-                if (numList.Count > 0)
-                {
-                    var message = "Negatives not allowed: ";
-                    message = String.Concat(message, String.Join(", ", numList));
-                    throw new ArgumentException(message);
-                }
+                sum = FindNegativeNumbers(sum, numbers, numList);
+                ThrowsException(numList);
                 return sum;
             }
             return 0;
+        }
+
+        private static void ThrowsException(List<int> numList)
+        {
+            if (numList.Count > 0)
+            {
+                var message = "Negatives not allowed: ";
+                message = String.Concat(message, String.Join(", ", numList));
+                throw new ArgumentException(message);
+            }
+        }
+
+        private static int FindNegativeNumbers(int sum, string[] numbers, List<int> numList)
+        {
+            foreach (string number in numbers)
+            {
+                Int32.TryParse(number, out var num);
+                if (num < 0) numList.Add(num);
+                sum = sum + num;
+            }
+            return sum;
         }
     }
 }
